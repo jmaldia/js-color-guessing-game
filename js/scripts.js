4155b71b2e405983cf231a7c8cd6cdbeb1cccda1
Jon Maldia <jon@maldia.net>
2019-10-29 14:19:41 -0400

    Add reset option

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// HELPER FUNCTIONS
 let changeColors = (color) => {
     for(let i = 0; i < colors.length; i++) {
         squares[i].style.backgroundColor = color;
@@ -25,11 +26,33 @@ let randomColor = () => {
     return `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
 }
 
+// VARIABLES
 let colors = generateRandomColors(6)
 let squares = document.querySelectorAll(".square");
 let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
 let message = document.getElementById("message");
+let h1 = document.querySelector("h1");
+let resetButton = document.getElementById("reset");
+
+// MAIN FUNCTION
+// reset
+resetButton.addEventListener("click", function() {
+
+    resetButton.textContent = "New Colors"
+    // generate new colors
+    colors = generateRandomColors(6);
+    // pick a random color from the array
+    pickedColor = pickColor();
+    // change colorDisplay to match picked color
+    colorDisplay.textContent = pickedColor;
+    // change colors of squares
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+    }
+
+    h1.style.backgroundColor = "#232323";
+})
 
 colorDisplay.textContent = pickedColor;
 
@@ -45,7 +68,9 @@ for (let i = 0; i < colors.length; i++) {
         // compare color to pickedColor
         if (clickedColor === pickedColor) {
             message.textContent = "Correct! You Won!";
+            h1.style.backgroundColor = clickedColor;
             changeColors(clickedColor);
+            resetButton.textContent = "Play Again?"
         } else {
             this.style.backgroundColor = "#232323";
             message.textContent = "Try again.";
